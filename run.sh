3e18b86f30fe7e09bc2635100f7f909ea3001da8
Rohitt Vashishtha <aero31aero@gmail.com>
2022-03-12 02:08:11 +0530

    Update the create.py file and add some requirements.

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 # simple script to be run by pm2 or other process manager
-source ~/python-zulip-api/zulip-api-py3-venv/bin/activate
-cd python-zulip-api/zulip_bots/zulip_bots
-zulip-run-bot dropbot -c ../../dropletrc
+cd /home/rohitt
+source python-zulip-api/zulip-api-py3-venv/bin/activate
+zulip-run-bot clones/dropbot/dropbot.py -c clones/dropbot/dropletrc
