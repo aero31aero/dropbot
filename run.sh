fb4c2bbdc5703333c09cd703663a49d0bae5a35f
Rohitt Vashishtha <aero31aero@gmail.com>
2022-03-12 02:53:23 +0530

    Update run script.

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #! /usr/bin/env bash
 # simple script to be run by pm2 or other process manager
-cd /home/rohitt
-source python-zulip-api/zulip-api-py3-venv/bin/activate
-zulip-run-bot clones/dropbot/dropbot.py -c clones/dropbot/dropletrc
+source ./env/bin/activate
+zulip-run-bot dropbot.py -c dropletrc
